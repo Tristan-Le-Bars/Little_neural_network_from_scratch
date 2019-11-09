812d7302e0ef1661bdd86bd47b98fa0ea1714b2a
Tristan Le Bars <47105496+Tristan-Le-Bars@users.noreply.github.com>
2019-11-09 18:43:39 +0100

    Update primitive_sorting_AI.py
    the  user of the software can now personalize the dataset, that mean that he can train this AI to predict the type of object between 2, whatever the objects are until they can be described with only 2 parameters.

--- primitive_sorting_AI.py
+++ primitive_sorting_AI.py
@@ -1,12 +1,48 @@
 import numpy as np
-                        
-input_dataset = np.array(([5,5],[2,1],[6,6],[3,5],[1,1],[2,1],[3,3],[3,4],[5,5]), dtype = float) #input values of the dataset
-output_dataset = np.array(([1],  [0],  [1],  [0],  [1],  [0],  [1],  [0]), dtype = float) # output values of the dataset: near 1 = squarre, near 0 = rectangle
 
-input_dataset = input_dataset/np.amax(input_dataset, axis=0) #put the values of the neurons between 0 an 1 for an easier readability
+example_num = int(input("how much example do you want to give to the AI ?\n"))
+input_dataset = np.zeros((example_num + 1, 2), dtype = float)
+output_dataset = np.zeros((example_num, 1), dtype = float)
 
-wanted_values = np.split(input_dataset,[8])[0] #split allow us to get only the wanted values, here : the 8 first
-predict = np.split(input_dataset,[8])[1]
+
+def filling_dataset ():
+    global input_dataset
+    global output_dataset
+    global example_num
+    i = 0
+    param1 = 0
+    param2 = 0
+    obj_type = -1
+    
+    while i < int(example_num):
+        param1 = input("Parameter 1 of the object n°" + str(i) + ": ")
+        param2 = input("Parameter 2 of the object n°" + str(i) + ": ")
+        obj_type = input("type of the object (0 or 1) :")
+        input_dataset[i,:] = [int(param1), int(param2)]
+        output_dataset[i,:] = [int(obj_type)]
+        i = i + 1
+    param1 = input("Parameter 1 of the object that the AI have to find the type : ")
+    param2 = input("Parameter 2 of the object that the AI have to find the type : ")
+    input_dataset[i,:] = [int(param1), int(param2)]
+    input_dataset = input_dataset/np.amax(input_dataset, axis = 0)
+
+
+def get_wanted_values ():
+    global input_dataset
+    global example_num
+
+    wanted_values = np.split(input_dataset, [example_num])[0]
+    return wanted_values
+
+def get_predict ():
+    global input_dataset
+    global example_num
+
+    predict = np.split(input_dataset, [example_num])[1]
+    return predict   
+
+
+    
 
 class Neural_Network(object): #creation of the neural network class
     def __init__(self):
@@ -31,7 +67,9 @@ class Neural_Network(object): #creation of the neural network class
     def sigmoid_prime(self, s): #compute the derivative of the sigmoid function
         return s * (1 - s)
 
-    def backward(self, wanted_values, output_dataset, output_values): #retro-propagation function
+    def backward(self, wanted_values, output_values): #retro-propagation function
+        global output_dataset
+
         #output neurons layer
         self.output_error = output_dataset - output_values #compute the margin of error
         self.output_delta = self.output_error * self.sigmoid_prime(output_values) #compute the delta of the margin of error
@@ -47,28 +85,34 @@ class Neural_Network(object): #creation of the neural network class
         self.W2 += self.hidden_values.T.dot(self.output_delta) #we add to the synaps weight between the hidden layer and the output layer
                                                           #the matrix product of the hidden values with the delta ou le margin of error of the output neurons layer
 
-    def training(self, wanted_values, output_dataset): #training function of the AI
+    def training(self, wanted_values): #training function of the AI
+        global output_dataset
+
         output_values = self.forward(wanted_values)
-        self.backward(wanted_values,output_dataset,output_values)
+        self.backward(wanted_values,output_values)
 
     def prediction(self): #print the prediciton of the AI after training
-        print("predicted values after training: ")
-        print("Input : \n" + str(predict))
-        print("Output : \n" + str(self.forward(predict)))
+        predict = get_predict ()
+        print("donnée prédite après entrainement: ")
+        print("Entrée : \n" + str(predict))
+        print("Sortie : \n" + str(self.forward(predict)))
 
         if(self.forward(predict) < 0.5):
-            print("The AI have found a rectangle \n")
+            print("L'IA a trouvé un rectangle \n")
         else:
-            print("The AI have found a squarre \n")
+            print("L'IA a trouvé un carré \n")
 
+
+filling_dataset()
+wanted_values = get_wanted_values()
 
 brain = Neural_Network()
 
-for i in range(50000): #training loop, we put in parameter number of training the number of training that we want our AI to do
-    print("training number " + str(i) + "\n")
-    print("output of the dataset: \n" + str(output_dataset))
-    print("\noutput found by the AI : \n" + str(np.matrix.round(brain.forward(wanted_values),2)))
+for i in range(30000): #training loop, we put in parameter number of training the number of training that we want our AI to do
+    print("entrainement n° " + str(i) + "\n")
+    print("Sortie du dataset: \n" + str(output_dataset))
+    print("\nSortie trouver par l'IA: \n" + str(np.matrix.round(brain.forward(wanted_values),2)))
     print("\n")
-    brain.training(wanted_values,output_dataset)
+    brain.training(wanted_values)
 
 brain.prediction()
